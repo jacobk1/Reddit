05920753bf9d02eda4a0275776dbd2ff7a5cd035
jacobk1 <77909387+jacobk1@users.noreply.github.com>
2021-11-19 20:20:03 -0500

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,41 @@
+def gradeTranslator(user)  :
+#pair of list storing data
+   grades = ['A','B','C','D','F']
+   score = [90,80,70,60,50]
+   user = str(user)
+   
+   #checks input type
+   if (user.isnumeric()) :
+   
+      user = int(user)#convert input to int
+      user = (user // 10) * 10#round to nearest 10s place
+      
+      if(user < 50) :#check if input is less than 50 
+         user = 50
+         
+      return [grades[score.index(user)]] #returns result as a list
+      
+      
+   else :
+   
+      user = user.upper()#converts string to all uppercase characters
+      upperbound = score[grades.index(user)] + 9 # finds the upper bound of all possible scores
+      lowerbound = score[grades.index(user)]
+      #checks for special cases and adjust accordingly, in programs with alot of special cases this would be its own function
+      if (user == 'A') :
+          upperbound += 1
+      elif (user == 'F') :
+         lowerbound = 0
+         
+      return [lowerbound,upperbound]#returns lower and upper bound as list
+      
+      
+#test
+result = gradeTranslator('c')
+print("Your score is between ", result[0], " and ", result[1],"\t\t test input was 'c'")
+result = gradeTranslator('a')
+print("Your score is between ", result[0], " and ", result[1],"\t test input was 'a'")
+result = gradeTranslator('F')
+print("Your score is between ", result[0], " and ", result[1],"\t\t test input was 'F'")
+result = gradeTranslator(73)
+print("Your grade is ", result[0],"\t" * 8, "test input was 73")
